f5cd48c8a540746fb24fc8dd173bb17225505c37
Timothy Coish <tdcoish@gmail.com>
2019-03-12 22:13:52 -0700

    Camera Freelook
    Partial update.

---
+++ Assets/Scripts/CAM_Freemove.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+/**********************************************************************************
+Camera free movement. Camera moves around with traditional 3D cam. If they press e they go 
+up, if they press q they go down.
+******************************************************************************** */
+public class CAM_Freemove : MonoBehaviour {
+    
+    [SerializeField]
+    private float       _LookSpeed = 2f;
+
+    [SerializeField]
+    private float       _TumbleSpeed = 1f;
+
+    [SerializeField]
+    private float       _HorizontalMovementSpeed = 10f;
+
+    [SerializeField]
+    private float       _VerticalMovementSpeed = 4f;
+
+    [SerializeField]
+    private bool        _FreezeRoll = true;
+
+    private bool        _Sprint = false;
+
+	[SerializeField]
+	private float 		_FlySpeed = 4f;
+
+	private Camera		mCam;
+    
+	private void Start(){
+		mCam = GetComponent<Camera>();
+	}
+
+    // attach a camera to whatever object you put this in.
+    private void Update(){
+        CameraRotations();
+        MoveCamera();
+        Orient();
+    }
+    
+    private void Orient(){
+        if(_FreezeRoll){
+            Vector3 rot = transform.rotation.eulerAngles;
+            rot.z = 0;
+
+            transform.rotation = Quaternion.Euler(rot);
+        }
+    }
+
+    private void MoveCamera(){
+
+		Vector3 vel = new Vector3();
+
+		// handle forwards and backwards.
+		if(Input.GetKey(KeyCode.W)){
+			vel = transform.forward * _FlySpeed;
+		}
+		if(Input.GetKey(KeyCode.S)){
+			vel = transform.forward * -_FlySpeed;
+		}
+
+		transform.position += vel * Time.deltaTime;
+	}
+    private void CameraRotations()
+    {
+        /// Rotates player according to mouse movement
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        transform.Rotate(0, mouseX * _LookSpeed, 0);
+        transform.Rotate(-mouseY * _LookSpeed, 0, 0);
+        transform.Rotate(0, 0, -mouseX * _TumbleSpeed);// Only valid if _FreezeRoll is true
+    }
+}
+
+
+
